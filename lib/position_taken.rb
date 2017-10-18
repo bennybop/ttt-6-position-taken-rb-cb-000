c24f63abf8f113818a723adc8e91995306c83479
bennybop <bennybop74@yahoo.com>
2017-10-18 22:35:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
-def position_taken?(board, index)
+def position_taken?(board, position)
 
-    
-  board[index] == !(" " || "")
+
+  board[position] == !(" " || "")
 
    end
